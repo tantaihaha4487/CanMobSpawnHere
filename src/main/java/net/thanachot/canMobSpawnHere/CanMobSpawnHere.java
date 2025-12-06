737e8e84312ff249c68510df31c01f7fd496c899
tantaihaha4487 <tantaiiottwo@gmail.com>
2025-12-07 00:26:43 +0700

    feat: implement initial CanMobSpawnHere plugin with block spawnability checks, light source detection, and event listeners

---
+++ src/main/java/net/thanachot/canMobSpawnHere/CanMobSpawnHere.java
@@ -0,0 +1,18 @@
+package net.thanachot.canMobSpawnHere;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CanMobSpawnHere extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        Patrica
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
